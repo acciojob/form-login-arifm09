2e536d8a62f9089c5d162b4b8fd3a719a518307e
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-03 16:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
+let submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", getFormvalue);
+
+submitBtn.style.backgroundColor = "lightblue";
+
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault();
+
+	let fname = document.querySelector('input[name="fname"]').value;
+	let lname = document.querySelector('input[name="lname"]').value;
+
+	alert(fname + " " +lname);
 
 }
